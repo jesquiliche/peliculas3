2de7cd6147939de06af62cfdc0dac6f4a7747c8d
Jesús Quintana <jesquiliche@hotmail.comm>
2023-04-15 20:56:46 +0200

    borrar

--- src/components/CargarGeneros.jsx
+++ src/components/CargarGeneros.jsx
@@ -24,27 +24,12 @@ const fetchPeliculas = async (id) => {
   }
 };
 
-const obtenerNombreCategoria = async (id) => {
-  try {
-    const respuesta = await axios.get(
-      `https://api.themoviedb.org/3/genre/${id}`,
-      {
-        params: {
-          api_key: "25349d5497c8655f081fc1abfbd5aa08",
-          language: "es-ES",
-        },
-      }
-    );
-    return respuesta.data.name;
-  } catch (error) {
-    console.log(error);
-  }
-};
-
 const CargarGeneros = async ({ id }) => {
-  const peliculas = await fetchPeliculas(id);
-  const nombre = await obtenerNombreCategoria(id);
+  
 
+
+  
+  const peliculas = await fetchPeliculas(id);
   return <ListaPeliculas data={peliculas.results} />;
 };
 
